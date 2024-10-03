5c0fcb74b96a3072a2b1ddc75da72618114ef0ac
italobrunkhorst <italobrunkhorst@gmail.com>
2024-10-03 20:05:05 -0300

    commit

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const summarySection = document.getElementById('summary');
     const summaryContent = document.getElementById('summary-content');
     
-    form.addEventListener('submit', function(event) {
+    form.addEventListener('submit', async function(event) {
         event.preventDefault();
 
         const destination = document.getElementById('destination').value;
@@ -29,5 +29,45 @@ document.addEventListener('DOMContentLoaded', () => {
         `;
 
         summarySection.hidden = false;
+
+        const coordinates = await getCoordinates(destination);
+
+        if (coordinates) {
+            const mapSection = document.getElementById('map');
+            mapSection.hidden = false;
+
+            const map = new google.maps.Map(document.getElementById('map-container'),{
+                center: { lat: coordinates.lat, lng: coordinates.lng},
+                zoom: 12
+            });
+
+            new google.maps.Marker({
+                position: {lat: coordinates.lat, lng: coordinates.lng},
+                map: map,
+                title: destination
+            });
+        }
     });
+    function initMap(){
+
+    }
+    async function getCoordinates(destination) {
+        const apiKey = 'AIzaSyCcAcZy5HSoyy1frqy9EQCZB1PEgF9EAE0';
+        const apiUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(destination)}&key=${apiKey}`;
+
+        try{
+            const response = await fetch(apiUrl);
+            const data = await response.json();
+            if (data.status === 'OK') {
+                const location = data.results[0].geometry.location;
+                return location;
+            }
+            else{
+                throw new Error('Localização não encontrada');
+            }
+        }catch(error){
+            console.error(error);
+            return null;
+        }
+    }
 });
